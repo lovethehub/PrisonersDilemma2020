e23266f77303add2a198205bb86ab3bed8a70553
PikaChewSenpai <61804706+PikaChewSenpai@users.noreply.github.com>
2020-03-10 12:57:29 -0700

    Update team14.py

--- team14.py
+++ team14.py
@@ -1,1 +1,12 @@
+team_name = 'Dragons Hoard'
+strategy_name = 'Gotta keep that #1'
+strategy_description = 'We steal when our score is lower or if they have stolen in the past 5 turns.'
 
+def move(my_history, their_history, my_score, their_score):
+  if len(my_history)==0:
+    return 'c'
+  else:
+    if my_score <= their_score:
+      return 'b'
+    else: 
+      return 'c'
